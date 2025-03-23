f84aab1d4e6563a0f084272a163f4a7ca63cbc2e
Martin Marsicano <martin.marsicano@owasp.org>
2025-03-22 23:10:01 -0300

    Fix font-size

--- src/sections/AntelTower.vue
+++ src/sections/AntelTower.vue
@@ -4,14 +4,14 @@ import antelAuditoriumImage from "@/assets/img/antel-auditorium.jpg";
 </script>
 
 <template>
-  <div class="flex-row align-content-center" style="display: flex;">
-    <div class="flex-column col-lg-6">
-      <img class="img-fluid p-3" :src="antelTowerImage" loading="lazy" />
-      <img class="img-fluid p-3" :src="antelAuditoriumImage" loading="lazy" />
+  <div class="flex-row align-content-center flexp-medium">
+    <div class="flex-column col-lg-6" style="display: flex; justify-content: center; align-items: center;">
+      <img class="img-fluid p-3" style="width: 100%" alt="Torre de las telecomunicaciones de ANTEL"  :src="antelTowerImage" loading="lazy" />
+      <img class="img-fluid p-3" alt="Auditorio de la Torre de las Comunicaciones de ANTEL" :src="antelAuditoriumImage" loading="lazy" />
     </div>
     <div class="flex-column col-lg-6" style="display: flex; justify-content: center; align-items: center;">
-      <h2 class="text-center" style="font-size: 80px; line-height: 1.3em; color: #9c8cfb;" >Auditorio de la Torre de las Comunicaciones de ANTEL</h2>
-      <h3 class="text-center" style="font-size: 70px; line-height: 1.3em;color: #F7F7F7;">Guatemala 1075, Montevideo, Uruguay</h3>
+      <h2 class="h0 text-center" style="color: #9c8cfb;" >Auditorio de la Torre de las Comunicaciones de ANTEL</h2>
+      <h3 class="h0 text-center" style="color: #F7F7F7;">Guatemala 1075, Montevideo, Uruguay</h3>
     </div>
   </div>
   <div class="text-white p-4">
@@ -30,3 +30,24 @@ import antelAuditoriumImage from "@/assets/img/antel-auditorium.jpg";
     <h4 class="text-white">{{ $t("venueDescription11") }}</h4>
   </div>
 </template>
+
+<style>
+@media (max-width: 990px) {
+  .h0 {
+    font-size: 2rem !important;
+  }
+}
+@media (min-width: 990px) {
+  .flexp-medium {
+    display: flex;
+  }
+  .h0 {
+    font-size: calc(2.25rem + 3vw);
+  }
+}
+@media (min-width: 2100px) {
+  .h0 {
+    font-size: calc(3rem + 4vw);
+  }
+}
+</style>
