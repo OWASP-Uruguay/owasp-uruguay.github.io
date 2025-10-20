c1b68930a339a22b4763a88469b254db628a13f6
Martin Marsicano <martin.marsicano@owasp.org>
2025-10-20 19:23:56 -0300

    Training & Speakers Grids

--- src/sections/TrainingGrid.vue
+++ src/sections/TrainingGrid.vue
@@ -21,28 +21,28 @@ import speakerImg5 from "@/assets/img/speakers/Angel_Santa_Cruz.jpg"
       <div class="row">
         <div class="col-lg-6 col-12">
           <HorizontalTeamCard
-              class="mt-4"
-              :profile="{ name: 'Aram Hovsepyan', image: `${speakerImg2}` }"
-              :conference="{ title: 'OWASP SAMM in Practice', level: 'Introductory and overview', color: 'success',
-                description: 'trainingAramDescription' }"
-          />
-        </div>
-        <div class="col-lg-6 col-12">
-          <HorizontalTeamCard
               class="mt-lg-4 mt-5"
               :profile="{ name: 'Fabio Cerullo', image: `${speakerImg3}` }"
               :conference="{ title: 'Secure Coding for Large Language Model Applications', level: 'Intermediate', color: 'warning',
                 description: 'trainingFabioDescription' }"
           />
         </div>
-      </div>
-      <div class="row mt-4">
         <div class="col-lg-6 col-12">
           <HorizontalTeamCard
               class="mt-4 z-index-2"
               :profile="{ name: 'Jose Luis Zamora', image: `${speakerImg4}` }"
               :conference="{ title: 'Capacitador Experto en Ciberseguridad', level: 'Introductory and overview', color: 'success',
                 description: 'trainingJoseDescription' }"
+          />
+        </div>
+      </div>
+      <div class="row mt-4">
+        <div class="col-lg-12 col-12">
+          <HorizontalTeamCard
+              class="mt-4"
+              :profile="{ name: 'Aram Hovsepyan', image: `${speakerImg2}` }"
+              :conference="{ title: 'OWASP SAMM in Practice', level: 'Introductory and overview', color: 'success',
+                description: 'trainingAramDescription' }"
           />
         </div>
       </div>
